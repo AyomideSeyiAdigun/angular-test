85d776de7754a3b86be177a49b198854f7eb5f0b
AyomideSeyiAdigun <ayomideadigun2014@gmail.com>
2022-03-06 21:13:01 -0800

    3 files modified, 3 deleted

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,22 +6,19 @@ import {
   SimpleChanges,
 } from '@angular/core';
 
-
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css'],
   inputs: ['stateChange'],
 })
-
 export class TodoComponent implements OnInit {
-
- 
   @Input() changingStatus: String;
   statusMeassage: string;
   constructor() {}
   ngOnChanges(changes: SimpleChanges) {
     if (this.changingStatus === 'completed') {
+      document.getElementById('status').style.color = '#15e623';
       this.statusMeassage = ' Completed';
     }
   }
